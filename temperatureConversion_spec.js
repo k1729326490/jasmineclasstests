555e267f4f31f68f85a6f7430ee1a56b7c042ed6
k1729326490 <118305262+k1729326490@users.noreply.github.com>
2024-03-08 10:18:18 +0800

    Create temperatureConversion_spec.js

---
+++ temperatureConversion_spec.js
@@ -0,0 +1,17 @@
+
+
+const fahrenheitToCelsius = require('./temperatureConversion');
+
+describe('fahrenheitToCelsius', () => {
+    it('should convert boiling temperature correctly', () => {
+        expect(fahrenheitToCelsius(212)).toBe(100);
+    });
+
+    it('should convert freezing temperature correctly', () => {
+        expect(fahrenheitToCelsius(32)).toBe(0);
+    });
+
+    it('should convert room temperature correctly', () => {
+        expect(fahrenheitToCelsius(70)).toBeCloseTo(21.11, 2); 
+    });
+});

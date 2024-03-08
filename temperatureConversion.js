e17cc0298b8360712c1ba01260c5e308f61f4b0e
k1729326490 <118305262+k1729326490@users.noreply.github.com>
2024-03-08 10:41:47 +0800

    Update temperatureConversion.js

--- temperatureConversion.js
+++ temperatureConversion.js
@@ -1,8 +1,7 @@
-// temperatureConversion.js
 
 
 function fahrenheitToCelsius(fahrenheit) {
     return (fahrenheit - 32) * (5/9);
 }
 
-module.exports = fahrenheitToCelsius;
+export {fahrenheitToCelsius}
